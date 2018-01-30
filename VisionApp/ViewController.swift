6ae0ff481755552ecf32d19d9d820fd580f3e9f2
Melodie Benmouffek <m.benmouffek@gmail.com>
2018-01-30 10:02:06 +0100

    Initial Commit

---
+++ VisionApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VisionApp
+//
+//  Created by Mélodie Benmouffek on 30/01/2018.
+//  Copyright © 2018 Mélodie Benmouffek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
