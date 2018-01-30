4ef7c297f15fdb1a29219d05f0a517e2403f46b1
Melodie Benmouffek <m.benmouffek@gmail.com>
2018-01-30 14:58:29 +0100

    Create flash state enumeration

---
+++ VisionApp/Enumerations/FlashState.swift
@@ -0,0 +1,14 @@
+//
+//  FlashState.swift
+//  VisionApp
+//
+//  Created by Mélodie Benmouffek on 30/01/2018.
+//  Copyright © 2018 Mélodie Benmouffek. All rights reserved.
+//
+
+import Foundation
+
+enum FlashState {
+    case on
+    case off
+}
