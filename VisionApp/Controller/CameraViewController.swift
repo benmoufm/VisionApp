16c0fd505f7977a6bc8110064b25843c7e7ece29
Melodie Benmouffek <m.benmouffek@gmail.com>
2018-01-30 12:19:59 +0100

    Add camera sub layer to camera view

--- VisionApp/Controller/CameraViewController.swift
+++ VisionApp/Controller/CameraViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class CameraViewController: UIViewController {
     //MARK: - Outlets
@@ -17,8 +18,42 @@ class CameraViewController: UIViewController {
     @IBOutlet weak var capturedImageView: RoundedShadowImageView!
     @IBOutlet weak var flashButton: RoundedShadowButton!
 
+    //MARK: - Variables
+    var captureSession: AVCaptureSession!
+    var cameraOutput: AVCapturePhotoOutput!
+    var previewLayer: AVCaptureVideoPreviewLayer!
+
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        previewLayer.frame = cameraView.bounds
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        captureSession = AVCaptureSession()
+        captureSession.sessionPreset = .hd1920x1080
+        let backCamera = AVCaptureDevice.default(for: .video)
+        do {
+            let input = try AVCaptureDeviceInput(device: backCamera!)
+            if captureSession.canAddInput(input) {
+                captureSession.addInput(input)
+            }
+            cameraOutput = AVCapturePhotoOutput()
+            if captureSession.canAddOutput(cameraOutput) {
+                captureSession.addOutput(cameraOutput!)
+            }
+            previewLayer = AVCaptureVideoPreviewLayer(session: captureSession!)
+            previewLayer.videoGravity = .resizeAspect
+            previewLayer.connection?.videoOrientation = .portrait
+            cameraView.layer.addSublayer(previewLayer!)
+            captureSession.startRunning()
+        } catch {
+            debugPrint(error as Any)
+        }
     }
 }
 
